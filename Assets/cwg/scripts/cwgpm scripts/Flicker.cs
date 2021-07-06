8605a9006a78e6ea8225bf998f336b725ebfcb25
Nervana <jeremyboydell@gmail.com>
2021-07-06 02:03:59 -0700

    toilet fixed
    sprites cycle beautifully now, no animators.

---
+++ Assets/cwg/scripts/cwgpm scripts/Flicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : Interactable
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+    public int currentSprite;
+
+    protected override void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            Debug.Log("interacted");
+            ChangeSprite();
+        }
+    }
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteArray[currentSprite];
+        currentSprite++;
+
+        if (currentSprite >= spriteArray.Length)
+        {
+            currentSprite = 0;
+        }
+    }
+}

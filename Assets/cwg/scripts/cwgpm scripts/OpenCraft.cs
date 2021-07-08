f7618acafbde4db6374fdf6d22e3f429d16684ed
Nervana <jeremyboydell@gmail.com>
2021-07-08 01:17:06 -0700

    scene transition on interact
    on the cutting board

--- Assets/cwg/scripts/cwgpm scripts/OpenCraft.cs
+++ Assets/cwg/scripts/cwgpm scripts/OpenCraft.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class OpenCraft : MonoBehaviour
+public class OpenCraft : Interactable
+
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoxCollider2D physicsCollider;
+
+    [Header("New Scene Variables")]
+    public string sceneToLoad;
+    public Vector2 playerPosition;
+    public VectorValue playerMemory;
+
+    [Header("Scene Transition Style")]
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    private void Awake()
     {
-        
+        if (fadeInPanel != null)
+        {
+            //awake happens before anything else
+            //if the fadeInPanel doesn't already exist, instantiate it in a place
+
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            //destroy the fadein after 1 second
+            Destroy(panel, 1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Interact()
     {
-        
+                if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+                {
+                    playerMemory.initialValue = playerPosition;
+                    StartCoroutine(FadeCo());
+                }
+                else
+                    Debug.Log("space not pressed");
+                //SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+
     }
 }
